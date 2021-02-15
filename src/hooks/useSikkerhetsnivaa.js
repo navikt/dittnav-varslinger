bd93f295bb1fe526aa0720b18d4048b06f4dd5ba
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-02-15 19:58:41 +0100

    Riktig bruk av intl

--- src/hooks/useSikkerhetsnivaa.js
+++ src/hooks/useSikkerhetsnivaa.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import { Innlogging } from "../constants";
 
 const useSikkerhetsnivaa = (brukernotifikasjon, type, innloggingsstatus) => {
+  const intl = useIntl();
   const skalMaskeres = brukernotifikasjon.sikkerhetsnivaa === 4 && innloggingsstatus.authLevel === 3;
-
-  const tekst = skalMaskeres ? <FormattedMessage id={`${type}.maskert.tekst`} /> : brukernotifikasjon.tekst;
-
+  const tekst = skalMaskeres ? intl.formatMessage({ id: `${type}.maskert.tekst` }) : brukernotifikasjon.tekst;
   const lenke = skalMaskeres ? `${Innlogging.LOGINSERVICE_LEVEL_4_URL}` : brukernotifikasjon.link;
 
   return {
