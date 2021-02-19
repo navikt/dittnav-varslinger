008e14b61f65b8c580a154d43c69e29140b1eef7
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-02-19 10:44:05 +0100

    Fikset cors feil i nyere versjoner sentry

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
   worker.start();
 } else {
-  Sentry.init({ dsn: "https://db441fa696d940bbad301593197ddfe8@sentry.gc.nav.no/70" });
+  Sentry.init({
+    dsn: "https://db441fa696d940bbad301593197ddfe8@sentry.gc.nav.no/70",
+    autoSessionTracking: false,
+  });
 }
 
 ReactDOM.render(
